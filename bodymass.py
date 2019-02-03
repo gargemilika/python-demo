620cdcd2f711eee377967049605b55e5907718a7
damir <tipalogin@gmail.com>
2019-02-03 11:57:52 +0300

    rest files

---
+++ bodymass.py
@@ -0,0 +1,14 @@
+def Body_mass_index(weight, height): # объявление + указание параметров
+    # отступ обязателен
+    """
+    >>> Body_mass_index (106, 1.68) #doctest: +ELLIPSIS
+    37.55...
+    """
+    index = weight / (height * height )
+    return index
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+from lib import cashback
+
+print ('our program')
+# value = cashback(10_000)
+# print(value)
+print(cashback(10_000))
